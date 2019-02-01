98c203aed10a65c21f4e0e9d6ed0ae00f627a5ba
BlackMrx <scrillex@outlook.fr>
2019-02-01 18:47:05 +0100

    some minor changes

--- wesalni/appOne/models.py
+++ wesalni/appOne/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from phonenumber_field.modelfields import PhoneNumberField
+#from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-    phone_number = PhoneNumberField()
+    #phone_number = PhoneNumberField()
     def __str__(self):
         return self.username
     
