b7032941fb1bae2b992bbc9d59d0d4dbeb70a2a0
zohir19 <43654743+zohir19@users.noreply.github.com>
2019-02-05 16:50:50 +0100

    phonenumber_field added

--- wesalni/appOne/models.py
+++ wesalni/appOne/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-#from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     date_of_birth= models.DateField(auto_now=False)
-    #phone_number = PhoneNumberField()
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
     def __str__(self):
         return self.username
         
