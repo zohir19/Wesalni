ce97dd9f36681565d55e87936a1953798075cf49
zohir19 <43654743+zohir19@users.noreply.github.com>
2019-02-05 17:36:56 +0100

    update

--- wesalni/appOne/views.py
+++ wesalni/appOne/views.py
@@ -71,7 +71,10 @@ def register_page(request):
         username = form.cleaned_data.get("username")
         email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
-        new_user = User.objects.create_user(username, email, password)
+        birth_date=form.cleaned_data.get("date_of_birth")
+        phone=form.cleaned_data.get("phone")
+        #city=form.cleaned_data.get("city")
+        new_user = User.objects.create_user(username, email, password,birth_date,phone)
         print(new_user)
     return render(request, "appOne/register.html", contexte)
 
