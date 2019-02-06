3554c22b33a2d2fc52cb3017105f78af4ca40b70
zohir19 <43654743+zohir19@users.noreply.github.com>
2019-02-06 15:53:22 +0100

    update

--- wesalni/appOne/models.py
+++ wesalni/appOne/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-from django.contrib.auth.admin import User
 # Create your models here.
-
-
-class myUser(models.Model):
-
+class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     date_of_birth= models.DateField(auto_now=False)
-    #phone = PhoneNumberField(null=False, blank=False, unique=True)
-    #city = models.CharField(max_length=50, default="Tlemcen")
-
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    city =models.CharField(max_length=50)
+    
     def __str__(self):
         return self.username
